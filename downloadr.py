9b24381fac5664b396e23799aed3bdd7b85555ce
Mike Taylor <miket@mozilla.com>
2013-12-06 12:37:06 -0600

    PEP8

--- downloadr.py
+++ downloadr.py
@@ -1,50 +1,66 @@
 #!/usr/bin/python
 
-import os
-from time import gmtime, strftime
-import sys
-from urllib2 import HTTPError, URLError, urlopen
+from __future__ import print_function
 import hashlib
 import magic
+import os
+import requests
+import sys
+from time import gmtime, strftime
+# from urllib2 import HTTPError, URLError, urlopen
+
+REQUEST_HEADERS = {'User-Agent': ('Mozilla/5.0 (Android; Mobile; rv:25.0) ')
+                                  'Gecko/25.0 Firefox/25.0'),
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+    'Accept-Language': 'en-US,en;q=0.5',
+    'Accept-Encoding': 'gzip, deflate'
+    'Connection': 'keep-alive'
+}
+
 
 def createDir():
     dirname = "webdevdata.org-" + strftime("%Y-%m-%d-%H%M%S", gmtime())
     os.mkdir(dirname)
     return dirname
 
+
 def connect(url):
+    session = requests.Session()
+    session.max_redirects = 5
+    session.get(url,
     try:
-        f = urlopen("http://" + url);
-        return f;
+        f = urlopen("http://" + url)
+        return f
     except URLError, e:
         try:
-            f = urlopen("https://" + url);
-            return f;
+            f = urlopen("https://" + url)
+            return f
         except URLError, e:
             try:
-                f = urlopen("http://www." + url);
-                return f;
+                f = urlopen("http://www." + url)
+                return f
             except URLError, e:
                 try:
-                    f = urlopen("https://www." + url);
-                    return f;
+                    f = urlopen("https://www." + url)
+                    return f
                 except HTTPError, e:
-                    print "HTTPError:", e.code, url
+                    print("HTTPError:", e.code, url)
                 except URLError, e:
-                    print "URLError:", e.reason, url
+                    print("URLError:", e.reason, url)
+
 
 def downloadFile(url, dir):
     os.chdir(dir)
     url = url.strip()
     try:
-        print "Downloading: ", url
+        print("Downloading: ", url)
         if url.startswith("http://"):
             url = url[7:]
         if url.startswith("https://"):
             url = url[8:]
         urlhost = url.split("/")[0]
         urlpath = "/".join(url.split("/")[1:])
-        f = connect(urlhost);
+        f = connect(urlhost)
         hash = hashlib.md5()
         hash.update(url)
         dir = hash.hexdigest()[:2]
@@ -62,21 +78,22 @@ def downloadFile(url, dir):
             local_file.write(str(f.getcode()) + "\n" + str(f.info()))
             local_file.close()
     except HTTPError, e:
-        print "HTTPError:", e.code, url
+        print("HTTPError:", e.code, url)
     except URLError, e:
-        print "URLError:", e.reason, url
+        print("URLError:", e.reason, url)
 
-if __name__=="__main__":
+if __name__ == "__main__":
     if len(sys.argv) < 2:
-        print >>sys.stderr, "Usage:", sys.argv[0], "create|download", "<URL>", "<dir>"
+        print("Usage:", sys.argv[0], "create|download <URL> <dir>",
+              file=sys.stderr)
         quit()
     command = sys.argv[1]
     if command == "create":
-        print createDir()
+        print(createDir())
     elif command == "download":
         if len(sys.argv) < 4:
-            print >>sys.stderr, "Where's the URL and the directory?"
+            print("Where's the URL and the directory?", file=sys.stderr)
             quit()
         downloadFile(sys.argv[2], sys.argv[3])
     else:
-        print >>sys.stderr, "Didn't understand the command", command
+        print("Didn't understand the command", file=sys.stderr)
